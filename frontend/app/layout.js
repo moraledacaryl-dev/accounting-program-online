92109805303cae0ac809ace53940ba49c02d4c50
moraledacaryl-dev <moraledacaryl@gmail.com>
2026-08-09 01:26:18 +0800

    Load Pass 42 modal closure styles

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -44,6 +44,7 @@ import './pass-19-sidebar-refinement.css';
 import './pass-20-page-template-system.css';
 import './pass-33-sidebar-search-containment.css';
 import './pass-41-ui-closure.css';
+import './pass-42-modal-closure.css';
 import ConfirmActionProvider from '../components/ConfirmActionProvider';
 import AppFrame from '../components/app-shell/AppFrame';
 import { AppShellProvider } from '../components/app-shell/AppShellContext';
