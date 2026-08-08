cf6b82b35750bcdf63f74973b38a952a5f72255d
moraledacaryl-dev <moraledacaryl@gmail.com>
2026-08-09 01:36:03 +0800

    Hotfix: provide consistent input dialogs app-wide

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -46,6 +46,7 @@ import './pass-33-sidebar-search-containment.css';
 import './pass-41-ui-closure.css';
 import './pass-42-modal-closure.css';
 import ConfirmActionProvider from '../components/ConfirmActionProvider';
+import InputActionProvider from '../components/InputActionProvider';
 import AppFrame from '../components/app-shell/AppFrame';
 import { AppShellProvider } from '../components/app-shell/AppShellContext';
 import { CurrentUserProvider } from '../lib/useCurrentUser';
@@ -59,7 +60,9 @@ export default function RootLayout({ children }) {
         <CurrentUserProvider>
           <AppShellProvider>
             <ConfirmActionProvider>
-              <AppFrame>{children}</AppFrame>
+              <InputActionProvider>
+                <AppFrame>{children}</AppFrame>
+              </InputActionProvider>
             </ConfirmActionProvider>
           </AppShellProvider>
         </CurrentUserProvider>
