2d0dbc974bb74b32ec4b20a7b6c8d6afbd3fdbdd
moraledacaryl-dev <moraledacaryl@gmail.com>
2026-09-10 01:27:07 +0800

    Shorten supplier credit migration revision ID

--- backend/alembic/versions/0011_supplier_credit_applications.py
+++ backend/alembic/versions/0011_supplier_credit_applications.py
@@ -1,6 +1,6 @@
 """Supplier credit applications
 
-Revision ID: 0011_supplier_credit_applications
+Revision ID: 0011_supplier_credit_apps
 Revises: 0010_payable_adjustments
 Create Date: 2026-09-10
 """
@@ -9,7 +9,7 @@ from alembic import op
 import sqlalchemy as sa
 from sqlalchemy import inspect
 
-revision = '0011_supplier_credit_applications'
+revision = '0011_supplier_credit_apps'
 down_revision = '0010_payable_adjustments'
 branch_labels = None
 depends_on = None
